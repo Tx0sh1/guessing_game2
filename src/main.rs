edf4353b2c374941d4cf62eb5723552134ac41fd
Tx0sh1 <clydemojura@protonmail.com>
2025-08-27 21:19:03 +0200

    add loop and input parsing

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,21 @@ fn main() {
     println!("Hi, {name} welcome to the guessing game");
 
     let secret_number = rand::rng().random_range(1..=100);
-    println!("secret numer is: {secret_number}")
+    println!("secret numer is: {secret_number}");
+
+    loop {
+        let mut guess = String::new();
+        println!("Enter your guess:");
+
+        io::stdin().read_line(&mut guess).expect("failed");
+        let result :i32 = match guess.trim().parse() {
+            Ok(val) => val,
+            Err(_) => {
+                println!("invalid input. Enter a number");
+                continue;
+            }
+        };
+    }
+
 
 }
